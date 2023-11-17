83e05877d8e51efc645aa6f768b201c4ccc97272
Omar Maskati <70995663+omaskati@users.noreply.github.com>
2023-11-17 15:30:25 -0500

    Generate default 16x16 flex grid

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+const container = document.querySelector("#container");
+createGrid(16);
+
+// MAKE 16 x 16 DIV GRID
+function createGrid(size){
+    for (let i=0; i<size; i++){
+
+        let row = document.createElement('div');
+        row.classList.add("row");
+        container.appendChild(row);
+
+        for (let j = 0; j < size; j++){
+            let cell = document.createElement('div');
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+    }
+}
